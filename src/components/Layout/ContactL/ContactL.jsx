e96f6ff8118887967d373f744c072aca568af01d
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-02-14 00:50:45 -0500

    Se termino el diseño de la pagina 3

--- src/components/Layout/ContactL/ContactL.jsx
+++ src/components/Layout/ContactL/ContactL.jsx
@@ -5,16 +5,14 @@ import './ContactL.css'
 export const ContactL = () => {
   return (
     <div className='containerContact'>
-        <div className='title'>
-        <h1 >Stay in touch</h1>
-        </div>
         
+        <h1 className='title'>Stay in touch</h1>
         <div className='formulario'>
         <input className='text' type="text" placeholder='User'/>
             <input className='text' type="text"  placeholder='Company'/>
             <input className='text' type="email" placeholder='E-mail' />
             <input className='text' type="text"  placeholder='Phone'/>
-            <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+            <textarea className='message text' cols="30" rows="10" placeholder='Message'></textarea>
             </div>
         <Boton mensaje='Send'/>
         
